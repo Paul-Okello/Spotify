56dc9cc30b265e291513603d6222024865fae213
Paul-Okello <paulokello273@gmail.com>
2021-12-27 11:27:59 +0300

    ⚡ General Update:  Helper Function + SSR on index page

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,14 @@
+import { getSession } from "next-auth/react";
 import Head from "next/head";
 import Center from "../components/Center";
+import Player from "../components/Player";
 import Sidebar from "../components/Sidebar";
 
 export default function Home() {
   return (
     <div className='bg-black h-screen overflow-hidden '>
       <Head>
-        <title>Spotify</title>
+        <title>Spotify Clone</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
@@ -14,7 +16,16 @@ export default function Home() {
         <Sidebar />
         <Center />
       </main>
-      <div>{/* Player */}</div>
+      <div className='sticky bottom-0'>
+        <Player />
+      </div>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: { session },
+  };
 }
