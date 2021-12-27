521b3553081825a11fbd30ea267472e67713b1cd
Paul-Okello <paulokello273@gmail.com>
2021-12-27 11:24:44 +0300

    👌 IMPROVE: Added Recoil state

--- components/Center.js
+++ components/Center.js
@@ -1,7 +1,11 @@
 import { ChevronDownIcon } from "@heroicons/react/outline";
-import { useSession } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { useEffect, useState } from "react";
 import { shuffle } from "lodash";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { playlistIdState, playlistState } from "../atoms/playlistAtom";
+import useSpotify from "../hooks/useSpotify";
+import Songs from "./Songs";
 
 const colors = [
   "from-indigo-500",
@@ -31,16 +35,34 @@ const colors = [
 
 const Center = () => {
   const { data: session } = useSession();
+  const spotifyApi = useSpotify();
   const [color, setColor] = useState(null);
+  const playlistId = useRecoilValue(playlistIdState);
+  const [playlist, setPlaylist] = useRecoilState(playlistState);
+
+  console.log(playlist);
 
   useEffect(() => {
     setColor(shuffle(colors).pop());
-  }, []);
+  }, [playlistId]);
+
+  useEffect(() => {
+    spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body);
+      })
+      .catch((err) => {
+        console.log("Something went wrong   ", err);
+      });
+  }, [spotifyApi, playlistId]);
 
   return (
-    <div className='  flex-grow text-white'>
+    <div className='  flex-grow text-white h-screen overflow-y-scroll scrollbar-hide'>
       <header className='absolute top-5 right-8'>
-        <div className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-70 cursor-pointer rounded-full p-1 pr-2'>
+        <div
+          className='flex items-center bg-black space-x-3 opacity-90 hover:opacity-70 cursor-pointer rounded-full p-1 pr-2'
+          onClick={signOut}>
           <img
             src={session?.user.image}
             alt=''
@@ -52,10 +74,30 @@ const Center = () => {
       </header>
       <section
         className={`flex items-end space-x-7 bg-gradient-to-b to-black ${color} h-80 text-white p-8 `}>
-        <img src='' alt='' />
+        <img
+          src={playlist?.images?.[0]?.url}
+          alt=''
+          className='h-44 w-44 shadow-2xl'
+        />
+        <div className=''>
+          <p>PLAYLIST</p>
+          <h1 className='text-2xl md:text-3xl xl:text-5xl font-bold'>
+            {playlist?.name}
+          </h1>
+        </div>
       </section>
+      <div>
+        <Songs />
+      </div>
     </div>
   );
 };
 
 export default Center;
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context);
+  return {
+    props: { session },
+  };
+}
