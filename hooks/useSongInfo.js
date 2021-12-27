8cc5c6b657ecbf5bac1b64ae4ab081c3057f842a
Paul-Okello <paulokello273@gmail.com>
2021-12-27 11:27:19 +0300

    👽 Code Change:  Custom Hook

---
+++ hooks/useSongInfo.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { currentTrackIdState } from "../atoms/songAtom";
+import useSpotify from "./useSpotify";
+
+function useSongInfo() {
+  const [songInfo, setSongInfo] = useState(null);
+  const spotifyApi = useSpotify();
+  const [currentTrackId, setCurrentTrackId] =
+    useRecoilState(currentTrackIdState);
+
+  useEffect(() => {
+    const fetchSongInfo = async () => {
+      if (currentTrackId) {
+        const trackInfo = await fetch(
+          `https://api.spotify.com/v1/tracks/${currentTrackId}`,
+          {
+            headers: {
+              Authorization: `Bearer ${spotifyApi.getAccessToken()}`,
+            },
+          }
+        ).then((res) => res.json());
+        setSongInfo(trackInfo);
+      }
+    };
+    fetchSongInfo();
+  }, []);
+  return songInfo;
+}
+
+export default useSongInfo;
